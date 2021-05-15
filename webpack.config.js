85bf26ae5821e05b2e94393de9a0a4ef903521e1
Zutty <george.weller@gmail.com>
2021-05-15 20:13:56 +0100

    Add 04b_03 font and copy-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const CopyPlugin = require("copy-webpack-plugin");
 const config = require("@softwareventures/webpack-config");
 
 module.exports = config({
     title: "Deep Sea Salvage",
-    vendor: "dcgw"
+    vendor: "dcgw",
+    html: {
+        template: "index.html"
+    },
+    customize: configuration => ({
+        ...configuration,
+        plugins: [
+            ...configuration.plugins,
+            new CopyPlugin({
+                patterns: [{ from: "fonts", to: "" }]
+            })
+        ]
+    })
 });
